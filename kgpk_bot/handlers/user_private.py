5fc74c5a7b8498305ec82d302ea3b2dc6e9edd23
EXsiDe <petruhin.paulus@yandex.ru>
2024-03-25 02:22:17 +0300

    ''

--- kgpk_bot/handlers/user_private.py
+++ kgpk_bot/handlers/user_private.py
@@ -3,7 +3,9 @@ from aiogram import types, Router, F
 
 from filters.chat_types import ChatTypeFilter
 from keyboards.inline import create_inline_buttons
-from middleware.api_requests import get_groups
+from middleware.api_requests import get_groups, get_group
+
+import pandas as pd
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private']))
@@ -11,11 +13,34 @@ user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
+    group_buttons = dict()
     groups = await get_groups()
-    buttons = dict()
     for group in groups:
-        # TODO: придумать че сделать с айдишниками для коллбэков
-        buttons[group['groupName']] = group['groupName']
+        group_buttons[group['groupName']] = f'get_group_attendance_{group['groupName']}'
 
     await message.answer(text='Выберите группу:',
-                         reply_markup=create_inline_buttons(buttons=buttons, sizes=(4,)))
+                         reply_markup=create_inline_buttons(buttons=group_buttons, sizes=(4,)))
+
+
+@user_private_router.callback_query(F.data == 'back_to_groups')
+async def start_cmd_callback(callback: types.CallbackQuery):
+    group_buttons = dict()
+    groups = await get_groups()
+    for group in groups:
+        group_buttons[group['groupName']] = f'get_group_attendance_{group['groupName']}'
+    await callback.message.answer(text='Выберите группу:',
+                                  reply_markup=create_inline_buttons(buttons=group_buttons, sizes=(4,)))
+
+
+@user_private_router.callback_query(F.data.startswith('get_group_attendance'))
+async def get_group_attendance(callback: types.CallbackQuery):
+    group_name = callback.data.split('_')[-1]
+    group_data = await get_group(group_name)
+    pd.set_option('display.max_columns', None)
+    df = pd.json_normalize(group_data)
+    attendance_data = df['StudNumber'].value_counts().to_dict()
+    response = str()
+    back_button = {'Назад': 'back_to_groups'}
+    for i, j in attendance_data.items():
+        response += f'{i}: {j}\n'
+    await callback.message.answer(text=response, reply_markup=create_inline_buttons(buttons=back_button, sizes=(1,)))
--- kgpk_bot/main.py
+++ kgpk_bot/main.py
@@ -22,4 +22,5 @@ async def main():
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
